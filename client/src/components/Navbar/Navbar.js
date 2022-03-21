b1fa1bdacc6019c16e0bde95a5b66abe96319a26
AntipovIvan <21aw0102@jec.ac.jp>
2022-03-21 14:51:04 +0900

    testpush

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,18 +6,40 @@ import {
   Toolbar,
   Typography,
 } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import memories from '../../images/memories.png';
-
 import useStyles from './styles';
+import decode from 'jwt-decode';
 
 const Navbar = () => {
   const classes = useStyles();
-
   const [user, setUser] = useState(
     JSON.parse(localStorage.getItem('profile'))
   );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
 
+  const logout = () => {
+    dispatch({ type: 'LOGOUT' });
+
+    navigate('/');
+
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const token = user?.token;
+
+    // JWT
+    if (token) {
+      const decodedToken = decode(token);
+
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
+    setUser(JSON.parse(localStorage.getItem('profile')));
+  }, [location]);
   console.log(user);
 
   return (
@@ -60,6 +82,7 @@ const Navbar = () => {
               variant="contained"
               className={classes.logout}
               color="secondary"
+              onClick={logout}
             >
               Logout
             </Button>
