f9c6b0aa7c7c4224da8c818b3070bd99267b835e
AntipovIvan <21aw0102@jec.ac.jp>
2022-03-21 14:52:48 +0900

    Revert "google auth implement"
    This reverts commit 496dd901d7c8077646728624e7928fc3644c55ac.

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -2,6 +2,3 @@ export const CREATE = 'CREATE';
 export const UPDATE = 'UPDATE';
 export const DELETE = 'DELETE';
 export const FETCH_ALL = 'FETCH_ALL';
-
-export const AUTH = 'AUTH';
-export const LOGOUT = 'LOGOUT';
